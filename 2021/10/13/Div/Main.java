d2a50eb36403577285456c512675c22433d03766
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-10-13 08:24:03 +0900

    20211013

---
+++ 2021/10/13/Div/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    // A - Div
+    // https://atcoder.jp/contests/abc198/tasks/abc198_a
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        System.out.println(N - 1);
+    }
+} 
