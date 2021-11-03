83dcadb5fee1b52e0db81e7da4554ac876ab8449
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-11-03 23:19:24 +0900

    20211103

--- 2021/10/24/sample.js
+++ 2021/10/24/sample.js
@@ -3,11 +3,12 @@ var num = -1;
 
 slideshow_timer();
 function slideshow_timer() {
-    if(num == 2) {
+    if(num === 2) {
         num == 0;
     } else {
         num ++;
     }
     document.getElementById("mypic").src=pics_src[num];
-    setTimeout("slideshow_timer()", 3000);
 }
+setInterval(slideshow_timer, 3000);
+// setTimeout("slideshow_timer()", 3000);
