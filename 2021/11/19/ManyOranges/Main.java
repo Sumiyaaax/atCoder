4bc80b9316dd4d5c64033f4d2b5dade97fd27858
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-11-19 08:43:44 +0900

    20211119途中

---
+++ 2021/11/19/ManyOranges/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    // B - Many Oranges
+    // https://atcoder.jp/contests/abc195/tasks/abc195_b
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int W = sc.nextInt();
+        sc.close();
+    }
+} 
