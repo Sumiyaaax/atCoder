889c4589deed9d9289a2df2d5b0bb0d8b264b935
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2022-01-29 20:12:21 +0900

    20220129

---
+++ 2022/factoryMethod/idcard/IDCardFactory.java
@@ -0,0 +1,19 @@
+package idcard;
+import factory.*;
+import java.util.*;
+
+public class IDCardFactory extends Factory {
+    private Vector owners = new Vector();
+
+    protected Product createProduct(String owner) {
+        return new IDCard(owner);
+    }
+
+    protected void registerProduct(Product product) {
+        owners.add(((IDCard) product).getOwner());
+    }
+
+    public Vector getOwners() {
+        return owners;
+    }
+}
